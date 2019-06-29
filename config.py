231486a68d011b244c73e158500e4b3498cf4ea1
W. Patrick Jones <wpmjones@gmail.com>
2019-06-29 09:05:51 -0400

    PEP 8 updates

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import yaml
 
 with open('/home/tuba/config.yaml', 'r') as f:
-  settings = yaml.load(f)
+    settings = yaml.load(f)
 
 with open('/home/tuba/emoji.yaml', 'r') as f:
-  emojis = yaml.load(f)
+    emojis = yaml.load(f)
 
-def colorPick(r,g,b):
-  return (r*65536) + (g*256) + b
+
+def color_pick(r, g, b):
+    return int.from_bytes([r, g, b], byteorder='big')
